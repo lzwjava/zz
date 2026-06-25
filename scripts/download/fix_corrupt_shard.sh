5b88e6702c18be27f41ea6a9315d463a3f2f367a
lzwjava <lzwjava@gmail.com>
2026-06-26 00:11:49 +0800

    fix(download): repair corrupt shard handling script

--- scripts/download/fix_corrupt_shard.sh
+++ scripts/download/fix_corrupt_shard.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Re-download corrupt SEC-EDGAR shard
-wget -c -O /mnt/data/zz/datasets/sec-edgar/10-K/002137a1-6454-4dca-9582-93b9d177efde-90.parquet \
+DEST=/mnt/data/zz/datasets/sec-edgar/10-K/002137a1-6454-4dca-9582-93b9d177efde-90.parquet
+rm -f "$DEST"
+wget -O "$DEST" \
   "https://huggingface.co/datasets/kapilrao/SEC-EDGAR/resolve/main/10-K/002137a1-6454-4dca-9582-93b9d177efde-90.parquet"
